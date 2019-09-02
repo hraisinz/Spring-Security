f9f947cebdc8d70cab8cc9134ec84bfee9e28497
hraisinz <hraisinz@gmail.com>
2019-09-02 22:19:56 +0530

    Springboot security

--- src/main/java/com/securityservice/security/SecurityConfiguration.java
+++ src/main/java/com/securityservice/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.securityservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,7 +17,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user1")
                 .password("user123")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("user2")
+                .password("user789")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 
     @Bean
--- src/main/java/com/securityservice/security/UserResource.java
+++ src/main/java/com/securityservice/security/UserResource.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserResource {
 
-    @GetMapping("/home")
+
+    @GetMapping("/")
     public String getHome(){
         return ("<h2>Spring Security</h2>");
+    }
+
+    @GetMapping("/user")
+    public String getUser(){
+        return ("<h2>Hello User</h2>");
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return ("<h2>Hello Admin</h2>");
     }
 }
